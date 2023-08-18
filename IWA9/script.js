7134be55c8d699fb1b80796001a3b3b08ba8ba6c
SikelelwaKwinana <137270323+SikelelwaKwinana@users.noreply.github.com>
2023-08-18 02:21:15 +0200

    Update script.js

--- IWA9/script.js
+++ IWA9/script.js
@@ -1,25 +1,4 @@
-/*
-const rent = 400;
-const tax = '8%';
-const food = 51.7501;
-const salary = 800;
-const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
 
-// Only change below this line
-
-if (hourOfDay !== undefined && minuteOfDay !== undefined 
-    && hourOfDay === 0 && minuteOfDay === 0) 
-    {
-
-    const taxAsDecimal = parseFloat(tax) / 100; // Converting tax to a decimal
-    const startingAfterTax = salary * (1 - taxAsDecimal); // salary after tax
-    const balance = startingAfterTax - transport - food - rent; // Calculate the balance
-
-    console.log(`R ${balance.toFixed(2)}`); // Print balance with R symbol and 2 decimal places
-    */
-   
 // const salary = 4000;
 // const lodging = 'apartment'
 // const size = 'large'
