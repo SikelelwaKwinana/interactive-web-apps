77da4039082405f803ec992da7697ffe80933fe0
SikelelwaKwinana <137270323+SikelelwaKwinana@users.noreply.github.com>
2023-08-28 09:49:07 +0200

    Update script.js

--- IWA13/script.js
+++ IWA13/script.js
@@ -1,27 +1,31 @@
-let state = 'idle';
+let state = "idle";
 let user = null;
-let calculated = '1';
+let calculated = "1";
 
 // Only allowed to change below
 
-const logCalc = () => { 
-    const isString = typeof calculated === 'string' && !isNaN(calculated);
-    const calculatedAsNumber = isString ? parseInt(calculated) : parseInt(calculated);
-    calculated = calculatedAsNumber + 1;
-}
+const logCalc = () => {
+  //typeof determines the type of variable
+  const isString = typeof calculated === "string" && !isNaN(calculated); //!isNaN(calculated) to check if the string can be converted to a number
+  const calculatedAsNumber = isString
+    ? parseInt(calculated)
+    : parseInt(calculated);
+  calculated = calculatedAsNumber + 1;
+};
 
 const calcUser = () => {
-  logCalc();
-  if (calculated > 2) user = 'John';
-  if (calculated > 2) state = 'requesting';
-  if (calculated > 3) state = 'idle';
-}
+  logCalc(); //Call this function which increments the value of -calculated
+  if (calculated > 2) user = "John"; // if calculated is greater than 2, the user is set to 'John'
+  if (calculated > 2) state = "requesting"; //if calculated is greater than 2, the state is set to 'requesting'
+  if (calculated > 3) state = "idle"; //if calculated is greater than 3, the state is set back to 'idle'
+};
 
 const checkUser = () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`);
-	}
-}
+  if (user && state === "requesting") {
+    //if user is true and if state is 'requesting'.
+    console.log(`User: ${user} (${calculated})`);
+  }
+};
 
 // Only allowed to change code above
 
