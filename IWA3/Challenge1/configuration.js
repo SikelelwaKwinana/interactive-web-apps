20ce2647276269726a189782b354103bacaba9c6
SikelelwaKwinana <137270323+SikelelwaKwinana@users.noreply.github.com>
2023-09-14 00:16:55 +0200

    Delete IWA3 directory

--- IWA3/Challenge1/configuration.js
+++
@@ -1,4 +0,0 @@
-// Added export on variables
-
-export const company = 'ACME Inc.'
-export const year = 2022
--- IWA3/Challenge1/scripts.js
+++
@@ -1,9 +0,0 @@
-// From typo + curly brackets
-
-import { company } from './configuration.js' 
-import { year } from './configuration.js'
-
-const message = '© ' + company + ' (' + year + ')'
-document.querySelector('footer').innerText = message
-
-//console.log(message)
